84cb602ddcd59074e12989879d61134596b113a6
Kazuhito Suda <kazuhito@fisuda.jp>
2021-11-11 18:15:04 +0900

    Add examples for cygnus

---
+++ examples/cygnus/07.list-subscriptions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eu
+
+cd "$(dirname "$0")"
+cd ../..
+
+if [ ! -e .env ]; then
+  echo ".env file not fuond"
+  exit 1
+fi
+
+. ./.env
+
+ngsi list \
+  --host "${ORION}" \
+  --service openiot \
+  --path / subscriptions \
+  --pretty
