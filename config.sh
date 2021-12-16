597a90b053e3a078107b90441b8109e0124e20ce
Kazuhito Suda <kazuhito@fisuda.jp>
2021-12-16 21:04:26 +0900

    Bump: 0.8.0-next -> 0.9.0

--- config.sh
+++ config.sh
@@ -30,7 +30,7 @@ IDM_DEBUG=false
 IMAGE_KEYROCK=fiware/idm:8.1.0
 
 # Docker image for Postfix
-IMAGE_POSTFIX=letsfiware/postfix:0.8.0-next
+IMAGE_POSTFIX=letsfiware/postfix:0.9.0
 
 #
 # Wilma
@@ -271,7 +271,7 @@ NODE_RED_LOGGING_METRICS=
 NODE_RED_LOGGING_AUDIT=
 
 # Docker image for Node-RED
-IMAGE_NODE_RED=letsfiware/node-red:0.8.0-next
+IMAGE_NODE_RED=letsfiware/node-red:0.9.0
 
 #
 # Grafana
@@ -297,7 +297,7 @@ QUERYPROXY=
 QUERYPROXY_LOGLEVEL=info
 
 # Docker image for Queryproxy
-IMAGE_QUERYPROXY=letsfiware/queryproxy:0.8.0-next
+IMAGE_QUERYPROXY=letsfiware/queryproxy:0.9.0
 
 #
 # Tokenproxy
@@ -309,7 +309,7 @@ TOKENPROXY_LOGLEVEL=info
 TOKENPROXY_VERBOSE=
 
 # Docker image for Tokenproxy
-IMAGE_TOKENPROXY=letsfiware/tokenproxy:0.8.0-next
+IMAGE_TOKENPROXY=letsfiware/tokenproxy:0.9.0
 
 #
 # Regproxy
@@ -348,7 +348,7 @@ REGPROXY_LOGLEVEL=info
 REGPROXY_VERBOSE=false
 
 # Docker image for Regproxy
-IMAGE_REGPROXY=letsfiware/regproxy:0.8.0-next
+IMAGE_REGPROXY=letsfiware/regproxy:0.9.0
 
 #
 # MongoDB
