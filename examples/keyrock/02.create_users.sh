4b87445e2d58e1a2a4c46df61c53a02d68038b24
Kazuhito Suda <kazuhito@fisuda.jp>
2022-01-26 20:05:43 +0900

    Fix lint error

--- examples/keyrock/02.create_users.sh
+++ examples/keyrock/02.create_users.sh
@@ -6,7 +6,7 @@ DOMAIN=example.com
 
 for id in $(seq 10)
 do
- USER=$(printf "user%03d" $id)
+ USER=$(printf "user%03d" "$id")
  PASS=$(pwgen -s 16 1)
  ngsi users create --host "${HOST}.${DOMAIN}" --username "${USER}" --email "${USER}@${DOMAIN}" --password "${PASS}" > /dev/null
  echo "${USER}@${DOMAIN} ${PASS}"
