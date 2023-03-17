e698db2bdafa423ce9b6181754126c62b25e7e7b
Kazuhito Suda <kazuhito@fisuda.jp>
2023-03-17 21:14:10 +0900

    Bump: 0.24.0 -> 0.24.0-next

--- config.sh
+++ config.sh
@@ -30,7 +30,7 @@ IDM_DEBUG=false
 IMAGE_KEYROCK=letsfiware/idm:8.1.0
 
 # Docker image for Postfix
-IMAGE_POSTFIX=letsfiware/postfix:0.24.0
+IMAGE_POSTFIX=letsfiware/postfix:0.24.0-next
 
 #
 # Wilma
@@ -325,7 +325,7 @@ NODE_RED_LOGGING_METRICS=
 NODE_RED_LOGGING_AUDIT=
 
 # Docker image for Node-RED
-IMAGE_NODE_RED=letsfiware/node-red:0.24.0
+IMAGE_NODE_RED=letsfiware/node-red:0.24.0-next
 
 #
 # Grafana
@@ -351,7 +351,7 @@ ZEPPELIN=
 ZEPPELIN_DEBUG=
 
 # Docker image for Zeppelin
-IMAGE_ZEPPELIN=letsfiware/zeppelin:0.24.0
+IMAGE_ZEPPELIN=letsfiware/zeppelin:0.24.0-next
 
 #
 # Queryproxy
@@ -363,7 +363,7 @@ QUERYPROXY=
 QUERYPROXY_LOGLEVEL=info
 
 # Docker image for Queryproxy
-IMAGE_QUERYPROXY=letsfiware/queryproxy:0.24.0
+IMAGE_QUERYPROXY=letsfiware/queryproxy:0.24.0-next
 
 #
 # Tokenproxy
@@ -375,7 +375,7 @@ TOKENPROXY_LOGLEVEL=info
 TOKENPROXY_VERBOSE=
 
 # Docker image for Tokenproxy
-IMAGE_TOKENPROXY=letsfiware/tokenproxy:0.24.0
+IMAGE_TOKENPROXY=letsfiware/tokenproxy:0.24.0-next
 
 #
 # Regproxy
@@ -414,7 +414,7 @@ REGPROXY_LOGLEVEL=info
 REGPROXY_VERBOSE=false
 
 # Docker image for Regproxy
-IMAGE_REGPROXY=letsfiware/regproxy:0.24.0
+IMAGE_REGPROXY=letsfiware/regproxy:0.24.0-next
 
 #
 # MongoDB
