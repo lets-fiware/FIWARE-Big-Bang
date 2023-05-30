957808efeff2c2e343de8fd9bcd2181b6aaa9cb1
Kazuhito Suda <kazuhito@fisuda.jp>
2023-05-30 18:46:55 +0900

    Update Mintaka to 0.5.37

--- config.sh
+++ config.sh
@@ -72,7 +72,7 @@ MINTAKA=
 MINTAKA_EXPOSE_PORT=
 
 # Docker image for Mintaka
-IMAGE_MINTAKA=quay.io/fiware/mintaka:0.5.36
+IMAGE_MINTAKA=quay.io/fiware/mintaka:0.5.37
 
 # Set a password for Timescale DB. Default: automatically generated
 TIMESCALE_PASS=
