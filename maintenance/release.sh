796ad1a4c9f1a9d469866952aaade9307ecb43fb
Kazuhito Suda <kazuhito@fisuda.jp>
2023-09-17 09:47:37 +0900

    Improve release action

--- maintenance/release.sh
+++ maintenance/release.sh
@@ -31,17 +31,34 @@ set -ue
 cd "$(dirname "$0")"
 cd ..
 
-if ! [ "$(echo "${REF}" | grep "^refs/tags/v")" ]; then
+TAG=$(cat VERSION)
+TAG="v${TAG##*=}"
+VER=${TAG//v/}
+echo "TAG: ${TAG}"
+echo "VER: ${VER}"
+
+if echo "${TAG}" | grep -q "-next"
+then
   exit 0
 fi
 
+if git tag | grep -q "${TAG}"
+then
+  exit 0
+fi
+
+AUTHOR_NAME=$(jq -r '.pull_request.merged_by.login' "$GITHUB_EVENT_PATH")
+AUTHOR_EMAIL=$(jq -r '.pull_request.merged_by.email' "$GITHUB_EVENT_PATH")
+
+git config user.name "${AUTHOR_NAME}"
+git config user.email "${AUTHOR_EMAIL}"
+git tag "${TAG}"
+git push origin "${TAG}"
+
 NAME=${REPO##*/}
-TAG=${REF##*/}
 
 echo "NAME: ${NAME}"
 echo "REPO: ${REPO}"
-echo "REF: ${REF}"
-echo "TAG: ${TAG}"
 
 FILE=CHANGELOG.md
 LINES=$(grep -n "^##" -m 2 "${FILE}" | sed -e 's/:.*//g'| sed -z "s/\n/,/g" | sed "s/,$//")
@@ -51,7 +68,7 @@ echo "${CHANGE_LOG}"
 RES=$(curl -X POST \
      -H "Authorization: token ${GITHUB_TOKEN}" \
      -d "{ \"tag_name\": \"${TAG}\", \"name\": \"${NAME//-/ } ${TAG}\", \"body\": \"${CHANGE_LOG}\"}" \
-     https://api.github.com/repos/${REPO}/releases)
+     "https://api.github.com/repos/${REPO}/releases")
 
 # Create tar.gz file
 
@@ -74,13 +91,48 @@ tar czvf "${FILE}" "${DIR}"
 
 rm -fr "${DIR}"
 
-## Upload tgz file
+## Upload tar.gz file
 
 UPLOAD_URL=$(echo "${RES}" | jq '. | .upload_url' | tr -d '"')
 UPLOAD_URL="${UPLOAD_URL%%\{*}?name=${FILE}"
 echo "UPLOAD URL: ${UPLOAD_URL}"
 
-curl -L -X POST ${UPLOAD_URL} -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+curl -L -X POST "${UPLOAD_URL}" -H "Authorization: Bearer ${GITHUB_TOKEN}" \
      -H 'Accept: application/vnd.github+json' \
      -H 'Content-Type: application/gzip' \
      --data-binary "@${FILE}"
+
+rm -f "${FILE}"
+
+## Create -next branch
+
+git switch -c "${TAG}-next"
+git push origin "${TAG}-next"
+
+## Create branch
+git switch -c "release/${VER}_next"
+
+VER_SED=${VER//\./\\.}
+
+for FILE in VERSION lets-fiware.sh
+do
+  sed -i -e "s/${VER_SED}/${VER_SED}-next/" "${FILE}"
+done
+
+sed -i "1i ## FIWARE Big Bang v${VER_SED}-next\n" CHANGELOG.md
+
+sed -i -e "s/node-red:${VER_SED}/node-red:${VER_SED}-next/" config.sh
+sed -i -e "s/tokenproxy:${VER_SED}/tokenproxy:${VER_SED}-next/" config.sh
+sed -i -e "s/queryproxy:${VER_SED}/queryproxy:${VER_SED}-next/" config.sh
+sed -i -e "s/regproxy:${VER_SED}/regproxy:${VER_SED}-next/" config.sh
+sed -i -e "s/postfix:${VER_SED}/postfix:${VER_SED}-next/" config.sh
+sed -i -e "s/zeppelin:${VER_SED}/zeppelin:${VER_SED}-next/" config.sh
+sed -i -e "s/pwgen:${VER_SED}/pwgen:${VER_SED}-next/" config.sh
+
+git add .
+git commit -m "Bump: ${VER} -> ${VER}-next"
+git push origin "release/${VER}_next"
+
+## Create PR
+
+gh pr create --base "${TAG}-next" --head "release/${VER}_next" --title "Bump: ${VER} -> ${VER}-next" --body "This PR is a preparation for the next release."
